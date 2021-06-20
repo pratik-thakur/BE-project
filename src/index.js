05fda5d8bf3df7c47fda6d18c2ec8f4bfb1ed08a
pratik-thakur <pratikthakur421@gmail.com>
2021-06-20 11:09:10 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express')
 require('./db/mongoose')
+const cors = require('cors')
 
 const userRouter = require('./routers/user')
 const postRouter = require('./routers/posts')
@@ -8,6 +9,7 @@ const postRouter = require('./routers/posts')
 const app = express()
 const port = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(postRouter)
